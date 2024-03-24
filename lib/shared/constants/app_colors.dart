9d61353caf552ed2e9f59a6e5692c60cb470e474
Sarraj-Sahar <saharsarraj20.ss@gmail.com>
2024-03-24 01:55:35 +0100

    updated shared

--- lib/shared/constants/app_colors.dart
+++ lib/shared/constants/app_colors.dart
@@ -3,25 +3,24 @@ import 'package:flutter/material.dart';
 //TOFIX: change app colors
 class AppColors {
   /// App primary color
-  static const Color primary = Color(0xff199A8E);
+  static const Color primary = Color(0xffF16E00);
 
   /// App secondary color
-  static const Color secondary = Color(0xffdeebea);
-
-  /// App secondary color
-  static const Color accent = Color.fromARGB(255, 24, 255, 216);
+  static const Color accent = Color(0xffD26000);
 
   /// App white color
-  static const Color dirtyWhite = Color(0xffF4F4F4);
+  static const Color white = Color(0xffFFFFFF);
 
-  /// App main text black color
-  static const Color mainTextBlack = Color(0xff101623);
+  /// App black color
+  static const Color black = Color(0xff0F172A);
+
+  ///Buttons shadow color
+  static const Color shadow = Color(0xff0E0E0E);
+
+  //STYLE: to add alert dialogs background color
 
   /// App secondary color
   static const Color error = Color(0xffFC698C);
-
-  /// App black color
-  static const Color black = Color(0xff14171A);
 
   /// Light grey color
   static const Color lightGrey = Color(0xffAAB8C2);
--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,3 +1,6 @@
 export 'constants/app_colors.dart';
 export 'constants/app_sizes.dart';
 export 'constants/text_styles.dart';
+export 'utils/app_url.dart';
+export 'utils/shared_prefrences.dart';
+export 'utils/snack_message.dart';
