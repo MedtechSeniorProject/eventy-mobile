28383e55d4a3ad668d0d444fb30110596a9630c5
Sarraj-Sahar <saharsarraj20.ss@gmail.com>
2024-03-25 15:18:37 +0100

    added custom neubrutalist button

--- lib/features/auth/widgets/custom_button.dart
+++ lib/features/auth/widgets/custom_button.dart
@@ -1,25 +1,41 @@
-import 'package:eventy_mobile/shared/constants/app_colors.dart';
+import 'package:eventy_mobile/shared/shared.dart';
 import 'package:flutter/material.dart';
 
-Widget customButton(
-    {VoidCallback? tap,
-    bool? status = false,
-    String? text = 'Save',
-    BuildContext? context}) {
-  return GestureDetector(
-    onTap: status == true ? null : tap,
-    child: Container(
-      height: 48,
-      margin: const EdgeInsets.symmetric(vertical: 15),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-          color: status == false ? AppColors.primary : AppColors.lightGrey,
-          borderRadius: BorderRadius.circular(8)),
-      width: MediaQuery.of(context!).size.width,
-      child: Text(
-        status == false ? text! : 'Please wait...',
-        style: TextStyle(color: AppColors.accent, fontSize: 18),
+class MyCustomButton extends StatelessWidget {
+  MyCustomButton({super.key, this.isBoxShadowVisible, this.text, this.status});
+  late bool? status = false;
+  final bool? isBoxShadowVisible;
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    Dimensions(context);
+    return Center(
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        height: Dimensions.screenHeight! * 5,
+        width: Dimensions.screenWidth! * 45,
+        margin: const EdgeInsets.symmetric(horizontal: 40),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: status == false ? AppColors.white : AppColors.white,
+          border: Border.all(
+            color: Colors.black,
+            width: 2,
+          ),
+          boxShadow: isBoxShadowVisible!
+              ? <BoxShadow>[
+                  const BoxShadow(color: Colors.black, offset: Offset(-6, 6))
+                ]
+              : [],
+        ),
+
+        //text
+        child: Text(
+          status == false ? text! : 'Please wait...',
+          style: myBoldText(Dimensions.screenHeight! * 2),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
