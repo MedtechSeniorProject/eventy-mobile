76cdf5cd6f51616a2b30814c29693c5fda3d7ad1
Sarraj-Sahar <saharsarraj20.ss@gmail.com>
2024-03-25 17:31:19 +0100

    🎨updated snack message

--- lib/shared/utils/snack_message.dart
+++ lib/shared/utils/snack_message.dart
@@ -1,12 +1,30 @@
 import 'package:eventy_mobile/shared/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
-//STYLE: to fix snack message style
-void showMessage({String? message, BuildContext? context}) {
-  ScaffoldMessenger.of(context!).showSnackBar(SnackBar(
-      content: Text(
-        message!,
-        style: TextStyle(color: AppColors.black),
+void showMessage(
+    {required String message,
+    required Color color,
+    required IconData icon,
+    BuildContext? context}) {
+  ScaffoldMessenger.of(context!).showSnackBar(
+    SnackBar(
+      elevation: 0,
+      backgroundColor: color.withOpacity(0.7),
+      behavior: SnackBarBehavior.floating,
+      margin: const EdgeInsets.all(5),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Icon(icon),
+          ),
+          Text(
+            message,
+            style: const TextStyle(color: AppColors.black),
+          ),
+        ],
       ),
-      backgroundColor: AppColors.primary.withOpacity(0.4)));
+    ),
+  );
 }
