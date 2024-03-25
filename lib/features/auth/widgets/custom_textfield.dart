ea8e3d81749b76b89523a7baaa3604a60a809683
Sarraj-Sahar <saharsarraj20.ss@gmail.com>
2024-03-25 15:20:00 +0100

    updated custom textfields

--- lib/features/auth/widgets/custom_textfield.dart
+++ lib/features/auth/widgets/custom_textfield.dart
@@ -1,36 +1,53 @@
 import 'package:eventy_mobile/shared/constants/app_colors.dart';
+import 'package:eventy_mobile/shared/constants/app_sizes.dart';
+import 'package:eventy_mobile/shared/constants/text_styles.dart';
 import 'package:flutter/material.dart';
 
-Widget customTextField(
-    {String? title,
-    String? hint,
-    TextEditingController? controller,
-    int? maxLines = 1}) {
-  return Column(
-    children: [
-      Container(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          title!,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: AppColors.black,
+class MyCustomTextField extends StatelessWidget {
+  const MyCustomTextField({
+    super.key,
+    this.title,
+    this.hint,
+    this.controller,
+  });
+
+  final String? title;
+  final String? hint;
+  final TextEditingController? controller;
+  final int? maxLines = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    Dimensions(context);
+    return Padding(
+      padding: const EdgeInsets.only(top: 5),
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.centerLeft,
+            child: Text(title!,
+                style: myRegularText(Dimensions.screenWidth! * 3.5)),
           ),
-        ),
+          //
+          Container(
+            margin:
+                EdgeInsets.symmetric(vertical: Dimensions.screenHeight! * 1.5),
+            padding: const EdgeInsets.symmetric(vertical: 3, horizontal: 20),
+            decoration: BoxDecoration(
+              border: Border.all(color: AppColors.black, width: 1.2),
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: TextFormField(
+              controller: controller,
+              maxLines: maxLines,
+              decoration: InputDecoration(
+                  hintText: hint,
+                  hintStyle: myHintText(Dimensions.screenWidth! * 3.5),
+                  border: InputBorder.none),
+            ),
+          )
+        ],
       ),
-      Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(8),
-          color: AppColors.lightGrey,
-        ),
-        child: TextFormField(
-          controller: controller,
-          maxLines: maxLines,
-          decoration: InputDecoration(hintText: hint, border: InputBorder.none),
-        ),
-      )
-    ],
-  );
+    );
+  }
 }
