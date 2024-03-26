222544793e88a18cdf240614161ef8445e1261dd
Sarraj-Sahar <saharsarraj20.ss@gmail.com>
2024-03-26 01:17:42 +0100

    🎨 updated add attendee manually screen

--- lib/features/add_manually/screens/add_manually_screen.dart
+++ lib/features/add_manually/screens/add_manually_screen.dart
@@ -1,7 +1,7 @@
 import 'package:eventy_mobile/features/add_manually/providers/add_attendee_provider.dart';
 import 'package:eventy_mobile/features/auth/widgets/custom_button.dart';
 import 'package:eventy_mobile/features/auth/widgets/custom_textfield.dart';
-import 'package:eventy_mobile/shared/utils/snack_message.dart';
+import 'package:eventy_mobile/shared/shared.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,6 +15,9 @@ class AddManuallyScreen extends StatefulWidget {
 class _AddManuallyScreenState extends State<AddManuallyScreen> {
   final TextEditingController _name = TextEditingController();
   final TextEditingController _email = TextEditingController();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _nameFocusNode = FocusNode();
+  bool _isBoxShadowVisible = true;
 
   @override
   void dispose() {
@@ -25,61 +28,108 @@ class _AddManuallyScreenState extends State<AddManuallyScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Dimensions(context);
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Add Manually'),
+          centerTitle: true,
+          title: const Text('Add Attendee'),
         ),
-        body: Container(
-            padding: const EdgeInsets.all(15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("add attendee manually"),
-                customTextField(
-                  title: 'name',
-                  controller: _name,
-                  hint: 'Enter name',
-                ),
-                customTextField(
-                  title: 'email',
-                  controller: _email,
-                  hint: 'Enter  email',
-                ),
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              padding: EdgeInsets.only(
+                top: Dimensions.screenHeight! * 10,
+                left: Dimensions.screenWidth! * 14,
+                right: Dimensions.screenWidth! * 14,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text("Insert Details",
+                        style: myBoldText(Dimensions.screenWidth! * 6)),
+                  ),
+                  SizedBox(
+                    height: Dimensions.screenHeight! * 5,
+                  ),
 
-                ///Button
-                Consumer<AddAttendeeProvider>(
-                    builder: (context, addattendee, child) {
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    if (addattendee.resMessage != '') {
-                      showMessage(
-                          message: addattendee.resMessage, context: context);
+                  MyCustomTextField(
+                    node: _nameFocusNode,
+                    title: 'name',
+                    controller: _name,
+                    hint: 'Enter name',
+                  ),
 
-                      addattendee.clear();
-                    }
-                  });
-                  return customButton(
-                    text: 'Add',
-                    tap: () {
-                      if (_name.text.isEmpty || _email.text.isEmpty) {
+                  MyCustomTextField(
+                    node: _emailFocusNode,
+                    title: 'email',
+                    controller: _email,
+                    hint: 'Enter  email',
+                  ),
+                  SizedBox(
+                    height: Dimensions.screenHeight! * 8,
+                  ),
+
+                  //custom button
+                  Consumer<AddAttendeeProvider>(
+                      builder: (context, addattendee, child) {
+                    WidgetsBinding.instance.addPostFrameCallback((_) {
+                      if (addattendee.resMessage != '') {
                         showMessage(
-                            message: "All fields are required",
-                            context: context);
-                      } else {
-                        addattendee.AddAttendee(
-                            context: context,
-                            name: _name.text.trim(),
-                            email: _email.text.trim());
-                      }
-                    },
-                    context: context,
-                    status: addattendee.isLoading,
-                  );
-                }),
+                          message: addattendee.resMessage,
+                          context: context,
+                          color: AppColors.success,
+                        );
 
-                const SizedBox(
-                  height: 10,
+                        ///Clear the response message to avoid duplicate
+                        addattendee.clear();
+                      }
+                    });
+                    return GestureDetector(
+                      //
+                      onTapDown: (details) => setState(() {
+                        _isBoxShadowVisible = false;
+                        if (_name.text.isEmpty || _email.text.isEmpty) {
+                          showMessage(
+                              color: AppColors.error,
+                              message: "All fields are required",
+                              context: context);
+                        } else {
+                          addattendee.AddAttendee(
+                              context: context,
+                              name: _name.text.trim(),
+                              email: _email.text.trim());
+                        }
+                      }),
+                      //
+                      onTapUp: (details) => setState(() {
+                        _isBoxShadowVisible = true;
+                      }),
+                      //
+                      child: MyCustomButton(
+                        isBoxShadowVisible: _isBoxShadowVisible,
+                        text: 'Add Attendee',
+                        status: addattendee.isLoading,
+                      ),
+                    );
+                  }),
+                ],
+              ),
+            ),
+            if (!_nameFocusNode.hasFocus && !_emailFocusNode.hasFocus)
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Image(
+                  image: const AssetImage(
+                    "assets/splash_bottomRight.png",
+                  ),
+                  fit: BoxFit.cover,
+                  width: Dimensions.screenWidth! * 25,
                 ),
-              ],
-            )));
+              ),
+          ],
+        ));
   }
 }
